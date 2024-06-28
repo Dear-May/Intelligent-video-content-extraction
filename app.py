721a7a79d7fb64a1345911d4715ff6cfd46a3721
月月睡不醒 <121281524+Dear-May@users.noreply.github.com>
2024-06-28 23:56:35 +0800

    修复在flask环境下启动视频无法播放的问题

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 
+@app.route('/WatermarkRemoval')
+def WatermarkRemoval():
+    return render_template('WatermarkRemoval.html')
+
+
+@app.route('/subtitling')
+def subtitling():
+    return render_template('subtitling.html')
+
+
+@app.route('/ImageEnhancement')
+def ImageEnhancement():
+    return render_template('ImageEnhancement.html')
+
+
+@app.route('/VideoTargetTracking')
+def VideoTargetTracking():
+    return render_template('VideoTargetTracking.html')
+
+
 @app.route('/')
 @app.route('/index')
 def index():
